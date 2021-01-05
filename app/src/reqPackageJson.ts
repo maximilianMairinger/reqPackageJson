5fb74a28eb71bda70e5121fe1126fed816456fc3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-05 02:25:02 +0100

    migrate

--- app/src/reqPackageJson.ts
+++ app/src/reqPackageJson.ts
@@ -1,7 +1,23 @@
+import * as fs from "fs"
+import * as path from "path"
 
 
-export function reqPackageJson() {
-  
+let parsed: any
+function tryParse(file: string) {
+  try {
+    parsed = JSON.parse(fs.readFileSync(file).toString())
+    return true
+  }
+  catch(e) {
+    return false
+  }
 }
 
-export default reqPackageJson
+export default function packageJsonConfig() {
+  let attempt = __dirname
+  while (!fs.existsSync(path.join(attempt, "package.json")) || !tryParse(path.join(attempt, "package.json"))) {
+    attempt = path.join(attempt, "..")
+  }
+
+  return parsed
+}
