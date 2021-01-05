18733e44d9cc45246ceca09ea2e6121a0e207a97
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-05 02:26:15 +0100

    dirname

--- app/src/reqPackageJson.ts
+++ app/src/reqPackageJson.ts
@@ -13,8 +13,8 @@ function tryParse(file: string) {
   }
 }
 
-export default function packageJsonConfig() {
-  let attempt = __dirname
+export default function packageJsonConfig(dirname: string) {
+  let attempt = dirname
   while (!fs.existsSync(path.join(attempt, "package.json")) || !tryParse(path.join(attempt, "package.json"))) {
     attempt = path.join(attempt, "..")
   }
