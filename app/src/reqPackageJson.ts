06c393cdff87c7845ce3778951887aa6038b05fc
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-06 03:40:20 +0100

    move to caller-path

--- app/src/reqPackageJson.ts
+++ app/src/reqPackageJson.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs"
 import * as path from "path"
-import { callerDirname } from 'caller-dirname';
+import * as caller from 'caller-path';
 
 
 let parsed: any
@@ -14,8 +14,8 @@ function tryParse(file: string) {
   }
 }
 
-export function reqPackageJson(dirname?: string) {
-  let attempt = dirname !== undefined ? dirname : callerDirname()
+export function reqPackageJson(dirname: string = caller()) {
+  let attempt = dirname
   while (!fs.existsSync(path.join(attempt, "package.json")) || !tryParse(path.join(attempt, "package.json"))) {
     attempt = path.join(attempt, "..")
   }
