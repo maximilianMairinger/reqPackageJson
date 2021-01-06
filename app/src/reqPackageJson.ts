f3375eac408e2e37757658390ca505348db62e1e
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-06 03:24:10 +0100

    fix?

--- app/src/reqPackageJson.ts
+++ app/src/reqPackageJson.ts
@@ -14,8 +14,8 @@ function tryParse(file: string) {
   }
 }
 
-export function reqPackageJson(dirname: string = callerDirname()) {
-  let attempt = dirname
+export function reqPackageJson(dirname?: string) {
+  let attempt = dirname !== undefined ? dirname : callerDirname()
   while (!fs.existsSync(path.join(attempt, "package.json")) || !tryParse(path.join(attempt, "package.json"))) {
     attempt = path.join(attempt, "..")
   }
